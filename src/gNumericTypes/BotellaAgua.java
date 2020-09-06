399ed49fcdddbf6da792c01825899ab9be6dafb1
Edder <viejoeder@hotmail.com>
2020-09-05 23:46:41 -0500

    se agrega la funcion de getpositive

--- src/gNumericTypes/BotellaAgua.java
+++ src/gNumericTypes/BotellaAgua.java
@@ -6,12 +6,15 @@ public class BotellaAgua
 {
     static int numeroMinutos;
     static int numeroBotellas;
+    static int n;
 
     public static void main(String[] args)
     {
 
-       System.out.println("digite la cantidad de tiempo ");
-       numeroMinutos = Input.get_int();
+
+       //System.out.println("digite la cantidad de tiempo en la ducha ");
+       //numeroMinutos = Input.get_int();
+        numeroMinutos = getpositive();
         if(numeroMinutos >= 0)
         {
             numeroBotellas=numeroMinutos * 12;
@@ -20,6 +23,19 @@ public class BotellaAgua
         {
             System.out.println("Numero fallido");
         }
+
+
+    }
+
+    private static int getpositive ()
+    {
+        while (n < 1 )
+        {
+            System.out.println("digite el tiempo en la ducha ");
+            n=Input.get_int();
+
+        }
+            return n;
 
     }
 
