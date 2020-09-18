2a0d64b561e42478b448839cb084a02c4b3f1408
eder <viejoeder@hotmail.com>
2020-09-17 23:57:12 -0500

    Ejercico de Split, Trim, parseInt

--- src/jStrings/inicialesDificil.java
+++ src/jStrings/inicialesDificil.java
@@ -7,9 +7,23 @@ public class inicialesDificil {
 
     public static void main(String[] args) {
 
-        System.out.println("Ingrese el nombre: ");
+        System.out.println("Ingrese los nombres separados por '/' ");
         String name=Input.get_string();
-        name.split(" ");
+        Input.print(name.split("/"));
+        String[] letras= name.split("/");
+
+        System.out.println("Ingrese hora:minutos:segundos");
+        String cadena = Input.get_string();
+        String[] tiempo = cadena.split(":");
+        Input.print(" "+tiempo);
+
+        System.out.println("ingrese varios espacion y numeros");
+        String numberString = Input.get_string();
+        Input.print(numberString.trim()+"\n");
+
+        System.out.println("string para convertir a int ");
+        String numberSt = Input.get_string();
+        int number=Integer.parseInt(numberSt);
 
     }
 
